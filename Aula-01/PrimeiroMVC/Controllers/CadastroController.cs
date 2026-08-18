f333c18f0f0a235d453c3713713569095bcf3205
davipedroza-2026 <davi.pedroza@edu.senai.br>
2026-08-18 11:13:40 -0300

    Finalizado

---
+++ Aula-01/PrimeiroMVC/Controllers/CadastroController.cs
@@ -0,0 +1,13 @@
+using System.Net.NetworkInformation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC2026;
+
+public class  CadastroController : Controller
+{
+    public IActionResult Aluno()
+    {
+        return View();
+    }
+}
+
